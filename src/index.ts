eeeff27c4a3fe3a15a6b96d54595aeb593080133
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2025-08-06 00:21:24 +0200

    chore: cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { createRouter } from './worker'
 
 const ROUTES = {
-  '/starlight-plugin-icons/*': 'proxy:https://starlight-plugin-icons.hegyi-aron101.workers.dev//*',
+  '/starlight-plugin-icons/*': 'proxy:https://starlight-plugin-icons.hegyi-aron101.workers.dev/*',
 }
 
 export default createRouter(ROUTES)
