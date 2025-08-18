b6a6d9038f391ad64e4fca333ff75f0ca5701e61
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2025-08-18 19:08:37 +0200

    feat: use package from npm
    assets don't load rip

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import { createRouter } from './worker'
+import type { Env } from 'cf-path-router'
+import { handleRequest } from 'cf-path-router'
 
 const ROUTES = {
-  '/starlight-plugin-icons/*': 'proxy:https://starlight-plugin-icons.hegyi-aron101.workers.dev/*',
-}
+  // '/starlight-plugin-icons/*': 'proxy:https://starlight-plugin-icons.hegyi-aron101.workers.dev/*',
+  '/starlight-plugin-icons/*': 'proxy:http://localhost:4321/*',
+} as const
 
-export default createRouter(ROUTES)
+export default {
+  async fetch(request: Request): Promise<Response> {
+    const env: Env = { ROUTES: JSON.stringify(ROUTES) }
+    const response = await handleRequest(request, env)
+    return response || new Response('Not found', { status: 404 })
+  },
+}
